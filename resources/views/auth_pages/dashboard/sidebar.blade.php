7c7634a5a3903e35f9e31076f184a80f674a773f
Pham Minh Tuan <theloneranger241@gmail,com>
2023-03-27 11:42:24 +0700

    update add login, register, dashboard template

---
+++ resources/views/auth_pages/dashboard/sidebar.blade.php
@@ -0,0 +1,151 @@
+<div class="app-sidebar-menu overflow-hidden flex-column-fluid">
+    <!--begin::Menu wrapper-->
+    <div id="kt_app_sidebar_menu_wrapper" class="app-sidebar-wrapper hover-scroll-overlay-y my-5"
+         data-kt-scroll="true" data-kt-scroll-activate="true" data-kt-scroll-height="auto"
+         data-kt-scroll-dependencies="#kt_app_sidebar_logo, #kt_app_sidebar_footer"
+         data-kt-scroll-wrappers="#kt_app_sidebar_menu" data-kt-scroll-offset="5px"
+         data-kt-scroll-save-state="true" style="height: 641px;">
+        <!--begin::Menu-->
+        <div class="menu menu-column menu-rounded menu-sub-indention px-3" id="#kt_app_sidebar_menu"
+             data-kt-menu="true" data-kt-menu-expand="false">
+            <!--begin:Menu item-->
+            <div data-kt-menu-trigger="click" class="menu-item here show menu-accordion hiding">
+                <!--begin:Menu link-->
+                <span class="menu-link">
+											<span class="menu-icon">
+												<!--begin::Svg Icon | path: icons/duotune/general/gen025.svg-->
+												<span class="svg-icon svg-icon-2">
+													<svg width="24" height="24" viewBox="0 0 24 24" fill="none"
+                                                         xmlns="http://www.w3.org/2000/svg">
+														<rect x="2" y="2" width="9" height="9" rx="2"
+                                                              fill="currentColor"></rect>
+														<rect opacity="0.3" x="13" y="2" width="9" height="9" rx="2"
+                                                              fill="currentColor"></rect>
+														<rect opacity="0.3" x="13" y="13" width="9" height="9" rx="2"
+                                                              fill="currentColor"></rect>
+														<rect opacity="0.3" x="2" y="13" width="9" height="9" rx="2"
+                                                              fill="currentColor"></rect>
+													</svg>
+												</span>
+                                                <!--end::Svg Icon-->
+											</span>
+											<span class="menu-title">Dashboards</span>
+											<span class="menu-arrow"></span>
+										</span>
+                <!--end:Menu link-->
+                <!--begin:Menu sub-->
+
+                <!--end:Menu sub-->
+            </div>
+            <!--end:Menu item-->
+            <!--begin:Menu item-->
+            <div class="menu-item pt-5">
+                <!--begin:Menu content-->
+                <div class="menu-content">
+                    <span class="menu-heading fw-bold text-uppercase fs-7">Pages</span>
+                </div>
+                <!--end:Menu content-->
+            </div>
+            <!--end:Menu item-->
+            <!--begin:Menu item-->
+
+
+            <!--end:Menu item-->
+            <!--begin:Menu item-->
+            <div data-kt-menu-trigger="click" class="menu-item menu-accordion hover show">
+                <!--begin:Menu link-->
+                <span class="menu-link">
+											<span class="menu-icon">
+												<!--begin::Svg Icon | path: icons/duotune/communication/com013.svg-->
+												<span class="svg-icon svg-icon-2">
+													<svg width="24" height="24" viewBox="0 0 24 24" fill="none"
+                                                         xmlns="http://www.w3.org/2000/svg">
+														<path
+                                                            d="M6.28548 15.0861C7.34369 13.1814 9.35142 12 11.5304 12H12.4696C14.6486 12 16.6563 13.1814 17.7145 15.0861L19.3493 18.0287C20.0899 19.3618 19.1259 21 17.601 21H6.39903C4.87406 21 3.91012 19.3618 4.65071 18.0287L6.28548 15.0861Z"
+                                                            fill="currentColor"></path>
+														<rect opacity="0.3" x="8" y="3" width="8" height="8" rx="4"
+                                                              fill="currentColor"></rect>
+													</svg>
+												</span>
+                                                <!--end::Svg Icon-->
+											</span>
+											<span class="menu-title">Authentication</span>
+											<span class="menu-arrow"></span>
+										</span>
+                <!--end:Menu link-->
+                <!--begin:Menu sub-->
+                <div class="menu-sub menu-sub-accordion show" kt-hidden-height="564" style="">
+                    <!--begin:Menu item-->
+
+                    <!--end:Menu item-->
+
+
+                    <!--end:Menu item-->
+                </div>
+                <!--end:Menu sub-->
+            </div>
+            <!--end:Menu item-->
+            <!--begin:Menu item-->
+
+
+            <!--begin:Menu item-->
+            <div data-kt-menu-trigger="click" class="menu-item menu-accordion">
+                <!--begin:Menu link-->
+                <span class="menu-link">
+											<span class="menu-icon">
+												<!--begin::Svg Icon | path: icons/duotune/finance/fin001.svg-->
+												<span class="svg-icon svg-icon-2">
+													<svg width="24" height="24" viewBox="0 0 24 24" fill="none"
+                                                         xmlns="http://www.w3.org/2000/svg">
+														<path
+                                                            d="M20 19.725V18.725C20 18.125 19.6 17.725 19 17.725H5C4.4 17.725 4 18.125 4 18.725V19.725H3C2.4 19.725 2 20.125 2 20.725V21.725H22V20.725C22 20.125 21.6 19.725 21 19.725H20Z"
+                                                            fill="currentColor"></path>
+														<path opacity="0.3"
+                                                              d="M22 6.725V7.725C22 8.325 21.6 8.725 21 8.725H18C18.6 8.725 19 9.125 19 9.725C19 10.325 18.6 10.725 18 10.725V15.725C18.6 15.725 19 16.125 19 16.725V17.725H15V16.725C15 16.125 15.4 15.725 16 15.725V10.725C15.4 10.725 15 10.325 15 9.725C15 9.125 15.4 8.725 16 8.725H13C13.6 8.725 14 9.125 14 9.725C14 10.325 13.6 10.725 13 10.725V15.725C13.6 15.725 14 16.125 14 16.725V17.725H10V16.725C10 16.125 10.4 15.725 11 15.725V10.725C10.4 10.725 10 10.325 10 9.725C10 9.125 10.4 8.725 11 8.725H8C8.6 8.725 9 9.125 9 9.725C9 10.325 8.6 10.725 8 10.725V15.725C8.6 15.725 9 16.125 9 16.725V17.725H5V16.725C5 16.125 5.4 15.725 6 15.725V10.725C5.4 10.725 5 10.325 5 9.725C5 9.125 5.4 8.725 6 8.725H3C2.4 8.725 2 8.325 2 7.725V6.725L11 2.225C11.6 1.925 12.4 1.925 13.1 2.225L22 6.725ZM12 3.725C11.2 3.725 10.5 4.425 10.5 5.225C10.5 6.025 11.2 6.725 12 6.725C12.8 6.725 13.5 6.025 13.5 5.225C13.5 4.425 12.8 3.725 12 3.725Z"
+                                                              fill="currentColor"></path>
+													</svg>
+												</span>
+                                                <!--end::Svg Icon-->
+											</span>
+											<span class="menu-title">Blog</span>
+											<span class="menu-arrow"></span>
+										</span>
+                <!--end:Menu link-->
+                <div class="menu-sub menu-sub-accordion menu-active-bg">
+                    <!--begin:Menu item-->
+                    <div class="menu-item">
+                        <!--begin:Menu link-->
+                        <a class="menu-link" href="../../demo1/dist/pages/blog/home.html">
+													<span class="menu-bullet">
+														<span class="bullet bullet-dot"></span>
+													</span>
+                            <span class="menu-title">Blog Home</span>
+                        </a>
+                        <!--end:Menu link-->
+                    </div>
+                    <!--end:Menu item-->
+                    <!--begin:Menu item-->
+                    <div class="menu-item">
+                        <!--begin:Menu link-->
+                        <a class="menu-link" href="../../demo1/dist/pages/blog/post.html">
+													<span class="menu-bullet">
+														<span class="bullet bullet-dot"></span>
+													</span>
+                            <span class="menu-title">Blog Post</span>
+                        </a>
+                        <!--end:Menu link-->
+                    </div>
+                    <!--end:Menu item-->
+                </div>
+                <!--end:Menu sub-->
+            </div>
+            <!--end:Menu item-->
+            <!--begin:Menu item-->
+
+
+            <!--end:Menu item-->
+        </div>
+        <!--end::Menu-->
+    </div>
+    <!--end::Menu wrapper-->
+</div>
